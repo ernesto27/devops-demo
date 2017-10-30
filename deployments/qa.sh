c7c80443899172e8c5fcd1db5393ded459caf34c
ernesto ponce <ernesto@pigmalionstudios.com.ar>
2017-10-29 21:48:51 -0300

    Add public param to now command

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,6 +1,6 @@
 npm install -g now
 echo "Deployng..."
-URL=$(now --docker -t $NOW_TOKEN)
+URL=$(now --docker --public -t $NOW_TOKEN)
 echo $URL
 echo "running acceptance on $URL"
 curl --silent -L $URL
